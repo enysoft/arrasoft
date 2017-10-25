815792fc80eb7ab63edddbed9eb68f40c07a94a4
DESKTOP-AIF6G6T\User <enysoft@yahoo.com>
2017-10-25 10:21:27 +0300

    Altceva

--- test.php
+++ test.php
@@ -1,12 +1,10 @@
 <?php
 	primul test ceva nou
-<<<<<<< HEAD
 	am mai pus ceva de la Ionut
 	test 3 ionut
-=======
+
 	modificat de IonutBranch3 daaaaa
 	alta Modificare de la IonutBranch3
 
 	asa, acum sa vedem ce vede mada in fisier
->>>>>>> master
 ?>
