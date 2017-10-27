33b901ee9e10084386a6796d50cba34a16183e45
Dinca Madalina <madaara@gamil.com>
2017-10-27 13:27:21 +0300

    URL

--- test.php
+++ test.php
@@ -1,9 +1,3 @@
 <?php
-	primul test ceva nou
-	mada
-
-	altaceva cristi
-
-	altceva mada
-	altceva modificat din console
+	Lorem ipsum might have been in use since the 1500’s but it has definitely been in use since at least the 1960’s. Back then veteran graphic designers used letraset sheets each of different fonts and font sizes to make up pages of type. Some of these sheets came with the Lorem ipsum passage on them.
 ?>
